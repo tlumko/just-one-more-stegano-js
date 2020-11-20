38bab5388687ee180a046102f272807519126636
Artem Shymchuk <artem.shymchuk@eleks.com>
2020-11-21 00:13:29 +0200

    refactor background for non-consecutive pixel usage

--- background.js
+++ background.js
@@ -1,14 +1,18 @@
 const fs = require('fs')
+const crypto = require('crypto')
 const { parseImage } = require('./utils')
 
 module.exports = {
   createBackground,
 }
 
-async function createBackground(img) {
+async function createBackground(img, secret='nosecret') {
   const png = await parseImage(img)
   const buffer = png.data
+  const order = (new Array(buffer.length)).fill(0).map((_, index) => index)
+  const hash = crypto.createHash('sha1').update(secret).digest()
   let pos = 0
+  let index = -1
 
   return {
     _buffer: buffer,
@@ -27,16 +31,32 @@ async function createBackground(img) {
   }
 
   function next() {
-    const res = {
-      pos: pos,
-      val: buffer[pos]
+    index++
+
+    if (isAlpha(index)) {
+      index++
     }
 
-    pos++
+    const increment = hash[index%(hash.length)] * 100
+    pos += increment+1
+
+    if (pos >= order.length - 1) { //the last entry in buffer - alpha. skip it
+      pos = pos%(order.length)
+      const spaceLeft = order.length - index - 1 //last alpha
+      pos = pos%spaceLeft
+      pos += index
+    }
 
     if (isAlpha(pos)) {
       pos++
     }
+
+    const res = {
+      pos: order[pos],
+      val: buffer[order[pos]]
+    }
+
+    ;[order[index], order[pos]] = [order[pos], order[index]]
 
     return res
   }
